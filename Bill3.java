eb5b9a356a79bbd672866c66a939eae1c391e326
hugo boss <ohad.shoshanilevi@post.runi.ac.il>
2024-11-07 15:00:14 +0200

    done

--- Bill3.java
+++ Bill3.java
@@ -10,6 +10,6 @@ public class Bill3 {
 		int totalPrice = Integer.parseInt(args[3]);
 		Double splitPrice = totalPrice/3.0;
 		
-     System.out.println("Dear " + name3 + ", " + name2+ ", " + " and " + name1 + ":" + " pay " + Math.ceil(splitPrice) + " Shekels each" );	    
+     System.out.println("Dear " + name3 + ", " + name2+ ", " + "and " + name1 + ":" + " pay " + Math.ceil(splitPrice) + " Shekels each" );	    
 	}
 }
